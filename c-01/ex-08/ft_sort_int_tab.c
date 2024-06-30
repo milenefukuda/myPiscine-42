8f1bb3020a275da4c94718b6025b1a02d016be74
Milene Fukuda <milene.fukuda@gmail.com>
2024-06-30 19:30:33 +0100

    c-01 ex-08

--- c-01/ex-08/ft_sort_int_tab.c
+++ c-01/ex-08/ft_sort_int_tab.c
@@ -10,9 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
-#include <stdlib.h>
 
 void    ft_sort_int_tab(int *tab, int size)
 {
-    
+    int     i;
+    int     tmp;
+
+    i = 0;
+    while(i < size - 1)
+    {
+        if(tab[i + 1] < tab[i])
+        {
+            tmp = tab[i];
+            tab[i] = tab[i + 1];
+            tab[i + 1] = tmp;
+            i = 0;
+        }
+        else
+            i++;
+    }
+}
+
+int     main()
+{
+    int     tab[4] = {7, 1, 8, 4};
+    ft_sort_int_tab(tab, 4);
+
+    int     i;
+    i = 0;
+    while(i < 4)
+    {
+        printf("%d\n", tab[i]);
+        i++;
+    }
+    return (0);
 }
